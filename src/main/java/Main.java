159b3b6048687369e8d457c0e7ea3f66102094d4
phuthuydz2k3 <hnhtr1999@gmail.com>
2023-05-31 22:35:10 +0700

    new commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,18 @@
 import java.util.*;
 import Models.Model;
 import Scrapers.FestivalsScraper;
-
+import utilities.Config;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        List<Model> cacLeHoi;
         FestivalsScraper scrapeFestival = new FestivalsScraper();
-        String page = "https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i_Vi%E1%BB%87t" +
-                "_Nam#L%E1%BB%85_h%E1%BB%99i_l%E1%BB%9Bn_c%C3%A1c_t%E1%BB%89nh_th%C3%A0nh";
-        String fileName = "database/CacLeHoi.json";
+        String page = Config.LE_HOI_WEBPAGE;
+        String leHoiFileName = Config.LE_HOI_FILENAME;
+        List<Model> cacLeHoi;
 
         cacLeHoi = scrapeFestival.scrapePage(page);
-        scrapeFestival.writeModel(fileName, cacLeHoi);
+        scrapeFestival.writeModel(leHoiFileName, cacLeHoi);
     }
 }
---
+++ src/main/java/utilities/Config.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class Config
+{
+    public static final String LE_HOI_FILENAME = "database/CacLeHoi.json";
+    public static final String LE_HOI_WEBPAGE = "https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i" +
+            "_Vi%E1%BB%87t_Nam#L%E1%BB%85_h%E1%BB%99i" +
+            "_l%E1%BB%9Bn_c%C3%A1c_t%E1%BB%89nh_th%C3%A0nh";
+}
